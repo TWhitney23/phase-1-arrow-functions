fd7b9d5d2bd89bd4992523797804f3e3b24d8671
Travelle Whitney <travellewhitney@gmail.com>
2025-01-11 22:59:11 -0600

    completed lab

---
+++ index.js
@@ -0,0 +1,21 @@
+
+
+
+
+function divide(a = 2000, b = 100) {
+    const result = a / b;
+    console.log(`The result of dividing ${a} by ${b} is ${result}`);
+    return result;
+}
+
+const square = (number) => {
+    const result = number * number;
+    console.log(result)
+    return result
+}
+
+const add = (numbers) => {
+    const result = 3 + 4;
+    console.log(result)
+    return result
+}
